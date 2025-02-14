5ef25d725cfc91d52f72d4c6889625c3c08bcfce
0xKitsune <0xkitsune@protonmail.com>
2025-02-13 19:25:06 -0500

    fix deps, update main

--- src/main.rs
+++ src/main.rs
@@ -26,60 +26,19 @@ fn main() -> eyre::Result<()> {
         _ => unreachable!(),
     };
 
-    // let data = if let Some(file_path) = args.file {
-    //     let file_content = fs::read_to_string(file_path)?;
-    //     hex::decode(file_content.trim())?
-    // } else if let Some(data_str) = args.data {
-    //     hex::decode(data_str)?
-    // } else {
-    //     unreachable!();
-    // };
+    let data = if let Some(file_path) = args.file {
+        let file_content = fs::read_to_string(file_path)?;
+        hex::decode(file_content.trim())?
+    } else if let Some(data_str) = args.data {
+        hex::decode(data_str)?
+    } else {
+        unreachable!();
+    };
 
-    // println!("");
-    // println!("Original size: {:?}", data.len());
-    // let compressed_data = compress_brotli(&data, brotli_level)?;
-    // println!("Compressed size: {:?}", compressed_data.len());
-
-    let mut full_proofs = vec![];
-    let mut compressed_padded_proofs = vec![];
-    let mut compressed_proofs = vec![];
-
-    let num_proofs = 100_000;
-    for _ in 0..num_proofs {
-        let full_proof = (0..256).map(|_| rand::random::<u8>()).collect::<Vec<u8>>();
-
-        full_proofs.extend(&full_proof);
-
-        compressed_padded_proofs.extend(&full_proof[..128].to_vec());
-        compressed_padded_proofs.extend(vec![0u8; 128]);
-
-        compressed_proofs.extend(full_proof[..128].to_vec());
-    }
-
-    println!("Num proofs: {}", num_proofs);
-    println!("\nUncompressed full proofs size: {:?}", full_proofs.len());
-    let compressed_full_8 = compress_brotli(&full_proofs, brotli_level)?;
-    println!("Compressed full proofs size: {:?}", compressed_full_8.len());
-
-    println!(
-        "\nUncompressed 'compressed proofs' with padding size: {:?}",
-        compressed_padded_proofs.len()
-    );
-    let compressed_padded_4 = compress_brotli(&compressed_padded_proofs, brotli_level)?;
-    println!(
-        "Compressed 'compressed proofs' with padding size: {:?}",
-        compressed_padded_4.len()
-    );
-
-    println!(
-        "\nUncompressed 'compressed proof' size: {:?}",
-        compressed_proofs.len()
-    );
-    let compressed_no_padding_4 = compress_brotli(&compressed_proofs, brotli_level)?;
-    println!(
-        "Compressed 'compressed proof' size: {:?}",
-        compressed_no_padding_4.len()
-    );
+    println!("");
+    println!("Original size: {:?}", data.len());
+    let compressed_data = compress_brotli(&data, brotli_level)?;
+    println!("Compressed size: {:?}", compressed_data.len());
 
     Ok(())
 }
